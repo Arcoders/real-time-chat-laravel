611f4e7f152827765e667d68fbc9767b0739dfbb
arcoders <anonismaa@gmail.com>
2017-12-14 21:35:36 +0100

    added components for right side

--- resources/assets/js/components/left/left.vue
+++ resources/assets/js/components/left/left.vue
@@ -1,36 +1,6 @@
 <template>
     <div id="left_app">
 
-        <section class="left">
-
-            <div class="profile">
-                <img src="https://avatars.io/twitter/nada">
-                <div class="icons">
-                    <i class="fa fa-pencil" aria-hidden="true"></i>
-                    <i class="fa fa-user-plus" aria-hidden="true"></i>
-                    <i class="fa fa-users" aria-hidden="true"></i>
-                    <i class="fa fa-cog" aria-hidden="true"></i>
-                    <i class="fa fa-bell" aria-hidden="true"></i>
-                    <i class="fa fa-power-off" aria-hidden="true"></i>
-                </div>
-            </div>
-
-            <search></search>
-
-            <div class="wrap-filter">
-                <div class="link_filter">
-                    <a href="#">Groups</a>
-                </div>
-                <div class="link_filter">
-                    <a href="#">Private</a>
-                </div>
-            </div>
-
-            <div class="contact-list">
-                <groups></groups>
-            </div>
-
-        </section>
 
     </div>
 </template>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,8 +29,256 @@
 
             <div class="wrap">
 
-                <left></left>
-                <right></right>
+
+                <section class="left">
+
+                    <div class="profile">
+                        <img src="https://avatars.io/twitter/nada">
+                        <div class="icons">
+                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                            <i class="fa fa-user-plus" aria-hidden="true"></i>
+                            <i class="fa fa-users" aria-hidden="true"></i>
+                            <i class="fa fa-cog" aria-hidden="true"></i>
+                            <i class="fa fa-bell" aria-hidden="true"></i>
+                            <i class="fa fa-power-off" aria-hidden="true"></i>
+                        </div>
+                    </div>
+
+                    <search></search>
+
+                    <div class="wrap-filter">
+                        <div class="link_filter">
+                            <a href="#">Groups</a>
+                        </div>
+                        <div class="link_filter">
+                            <a href="#">Private</a>
+                        </div>
+                    </div>
+
+                    <div class="contact-list">
+                        <groups></groups>
+                    </div>
+
+                </section>
+
+
+                <section class="right">
+
+
+                    <div class="chat-head">
+                        <img alt="profilepicture" src="https://avatars.io/twitter/maryam">
+                        <div class="chat-name">
+                            <h1 class="font-name">Laravel</h1>
+                            <p class="font-online">Ismael, Fatima, Admin, Marta, victor...</p>
+                        </div>
+                        <i class="fa fa-whatsapp fa-lg" aria-hidden="true"></i>
+                    </div>
+
+
+                    <div class="wrap-chat">
+
+                        <div class="chat">
+
+                            <div class="chat-bubble me">
+                                <div class="my-mouth">
+                                    <img class="me_img" src="https://avatars.io/twitter/maryam">
+                                </div>
+                                <div class="content">قولي أحبك كي تزيد وسامتي فبغير حبك ما أكون جميلا</div>
+                                <div class="time">15:20</div>
+                            </div>
+
+                            <div class="chat-bubble you">
+                                <div class="your-mouth">
+                                    <img class="you_img" src="https://avatars.io/twitter/nada">
+                                </div>
+                                <div class="content">Awdi ghir m3a dnya</div>
+                                <div class="time">15:27</div>
+                            </div>
+
+                            <div class="chat-bubble me">
+                                <div class="my-mouth">
+                                    <img class="me_img" src="https://avatars.io/twitter/maryam">
+                                </div>
+                                <div class="content">قولي أحبك كي تزيد وسامتي فبغير حبك ما أكون جميلا</div>
+                                <div class="time">15:20</div>
+                            </div>
+
+                            <div class="chat-bubble you">
+                                <div class="your-mouth">
+                                    <img class="you_img" src="https://avatars.io/twitter/nada">
+                                </div>
+                                <div class="content">Awdi ghir m3a dnya</div>
+                                <div class="time">15:27</div>
+                            </div>
+
+
+                            <div class="chat-bubble me">
+                                <div class="my-mouth">
+                                    <img class="me_img" src="https://avatars.io/twitter/maryam">
+                                </div>
+                                <div class="content">قولي أحبك كي تزيد وسامتي فبغير حبك ما أكون جميلا</div>
+                                <div class="time">15:20</div>
+                            </div>
+
+                            <div class="chat-bubble you">
+                                <div class="your-mouth">
+                                    <img class="you_img" src="https://avatars.io/twitter/nada">
+                                </div>
+                                <div class="content">Awdi ghir m3a dnya</div>
+                                <div class="time">15:27</div>
+                            </div>
+
+                            <div class="chat-bubble me">
+                                <div class="my-mouth">
+                                    <img class="me_img" src="https://avatars.io/twitter/maryam">
+                                </div>
+                                <div class="content">قولي أحبك كي تزيد وسامتي فبغير حبك ما أكون جميلا</div>
+                                <div class="time">15:20</div>
+                            </div>
+
+                            <div class="chat-bubble you">
+                                <div class="your-mouth">
+                                    <img class="you_img" src="https://avatars.io/twitter/nada">
+                                </div>
+                                <div class="content">Awdi ghir m3a dnya</div>
+                                <div class="time">15:27</div>
+                            </div>
+
+                            <div class="chat-bubble me">
+                                <div class="my-mouth">
+                                    <img class="me_img" src="https://avatars.io/twitter/maryam">
+                                </div>
+                                <div class="content">قولي أحبك كي تزيد وسامتي فبغير حبك ما أكون جميلا</div>
+                                <div class="time">15:20</div>
+                            </div>
+
+                            <div class="chat-bubble you">
+                                <div class="your-mouth">
+                                    <img class="you_img" src="https://avatars.io/twitter/nada">
+                                </div>
+                                <div class="content">Awdi ghir m3a dnya</div>
+                                <div class="time">15:27</div>
+                            </div>
+
+
+                            <div class="chat-bubble me">
+                                <div class="my-mouth">
+                                    <img class="me_img" src="https://avatars.io/twitter/maryam">
+                                </div>
+                                <div class="content">قولي أحبك كي تزيد وسامتي فبغير حبك ما أكون جميلا</div>
+                                <div class="time">15:20</div>
+                            </div>
+
+                            <div class="chat-bubble you">
+                                <div class="your-mouth">
+                                    <img class="you_img" src="https://avatars.io/twitter/nada">
+                                </div>
+                                <div class="content">Awdi ghir m3a dnya</div>
+                                <div class="time">15:27</div>
+                            </div>
+
+                            <div class="chat-bubble me">
+                                <div class="my-mouth">
+                                    <img class="me_img" src="https://avatars.io/twitter/maryam">
+                                </div>
+                                <div class="content">قولي أحبك كي تزيد وسامتي فبغير حبك ما أكون جميلا</div>
+                                <div class="time">15:20</div>
+                            </div>
+
+                            <div class="chat-bubble you">
+                                <div class="your-mouth">
+                                    <img class="you_img" src="https://avatars.io/twitter/nada">
+                                </div>
+                                <div class="content">Awdi ghir m3a dnya</div>
+                                <div class="time">15:27</div>
+                            </div>
+
+                            <div class="chat-bubble me">
+                                <div class="my-mouth">
+                                    <img class="me_img" src="https://avatars.io/twitter/maryam">
+                                </div>
+                                <div class="content">قولي أحبك كي تزيد وسامتي فبغير حبك ما أكون جميلا</div>
+                                <div class="time">15:20</div>
+                            </div>
+
+                            <div class="chat-bubble you">
+                                <div class="your-mouth">
+                                    <img class="you_img" src="https://avatars.io/twitter/nada">
+                                </div>
+                                <div class="content">Awdi ghir m3a dnya</div>
+                                <div class="time">15:27</div>
+                            </div>
+
+                            <div class="chat-bubble me">
+                                <div class="my-mouth">
+                                    <img class="me_img" src="https://avatars.io/twitter/maryam">
+                                </div>
+                                <div class="content">قولي أحبك كي تزيد وسامتي فبغير حبك ما أكون جميلا</div>
+                                <div class="time">15:20</div>
+                            </div>
+
+                            <div class="chat-bubble you">
+                                <div class="your-mouth">
+                                    <img class="you_img" src="https://avatars.io/twitter/nada">
+                                </div>
+                                <div class="content">Awdi ghir m3a dnya</div>
+                                <div class="time">15:27</div>
+                            </div>
+
+                            <div class="chat-bubble me">
+                                <div class="my-mouth">
+                                    <img class="me_img" src="https://avatars.io/twitter/maryam">
+                                </div>
+                                <div class="content">قولي أحبك كي تزيد وسامتي فبغير حبك ما أكون جميلا</div>
+                                <div class="time">15:20</div>
+                            </div>
+
+                            <div class="chat-bubble you">
+                                <div class="your-mouth">
+                                    <img class="you_img" src="https://avatars.io/twitter/nada">
+                                </div>
+                                <div class="content">Awdi ghir m3a dnya</div>
+                                <div class="time">15:27</div>
+                            </div>
+
+                            <div class="chat-bubble me">
+                                <div class="my-mouth">
+                                    <img class="me_img" src="https://avatars.io/twitter/maryam">
+                                </div>
+                                <div class="content">قولي أحبك كي تزيد وسامتي فبغير حبك ما أكون جميلا</div>
+                                <div class="time">15:20</div>
+                            </div>
+
+                            <div class="chat-bubble you">
+                                <div class="your-mouth">
+                                    <img class="you_img" src="https://avatars.io/twitter/nada">
+                                </div>
+                                <div class="content">Awdi ghir m3a dnya</div>
+                                <div class="time">15:27</div>
+                            </div>
+
+                        </div>
+
+                        <div class="upload_foto">
+                            <div class="container_foto font-preview">
+                                <p>Subir imagen</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+                    <div class="wrap-message">
+                        <i class="fa fa-smile-o fa-lg" aria-hidden="true"></i>
+                        <div class="message">
+                            <input type="text" class="input-message" placeholder="Escribe un nuevo mensaje">
+                        </div>
+                        <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+                    </div>
+
+
+                </section>
 
             </div>
 
