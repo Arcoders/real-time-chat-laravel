92ce6f46368716d7da4c1c2e2493fb54866b7505
arcoders <anonismaa@gmail.com>
2018-01-04 06:07:34 +0100

    Get group with last message

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Message;
 use Illuminate\Support\Facades\Auth;
 
 class ChatsController extends Controller
@@ -9,8 +10,22 @@ class ChatsController extends Controller
 
     public function chatsList()
     {
+        $allGroups = array();
+
+        $groups = Auth::user()->groups;
+
+        foreach ($groups as $group):
+
+            $message = Message::where('group_id', $group->id)->get()->last();
+
+            $group = collect($group);
+
+            array_push($allGroups, $group->push($message));
+
+        endforeach;
+
         return response()->json([
-            'groups' => Auth::user()->groups,
+            'groups' => $allGroups,
             'friends' => Auth::user()->friends(true)
         ], 200);
     }
