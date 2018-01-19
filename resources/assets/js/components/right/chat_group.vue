b8900593df3cfff11c700fad16fbacd90ea577dd
arcoders <anonismaa@gmail.com>
2018-01-19 19:38:05 +0100

    using mixins to refactor

--- resources/assets/js/components/right/chat_group.vue
+++ resources/assets/js/components/right/chat_group.vue
@@ -1,48 +1,6 @@
 <template lang="pug">
-    transition(name='slide-fade')
-        #right_app(v-if='showChat', @mouseleave="mouseLeave", @mouseout="mouseOut")
 
-            .chat-head
-                avatar.img-head(:username='chatName', color='#fff', :src='avatar')
-                .chat-name
-                    h1.font-name {{ chatName }}
-                    p.font-online(v-if='onlineUsers')
-                        span(v-for='onlineUser in onlineUsers')
-                            | {{ onlineUser.name }}
-                            span.green_font &#8226;
-                    p.font-online(v-else) Online
-                        span.red_font &#8226;
-                i.fa.fa-whatsapp.fa-lg(aria-hidden='true')
-
-            .wrap-content
-
-                .dynamic_content.chat#chat(v-chat-scroll='')
-                    messages(:messages='messages', :user='user', :usersTyping="typing")
-
-                .upload_foto(v-if='uploadImage')
-
-                    .container_foto.font-preview(v-if='!photo')
-                        label.fileContainer
-                            button
-                                i.material-icons file_upload
-                            input(type='file',
-                                        name='foto',
-                                        v-on:change='onFileChange($event)',
-                                        ref='fileInput')
-
-                    .container_foto(v-else='')
-                        .preview-image
-                            img(alt='profilepicture', :src='photo')
-                            a(v-on:click='photo = null')
-                                i.material-icons clear
-
-            send(:user='user',
-                    v-on:errorMessages="pushErrorMessage($event)",
-                    v-on:clearPhoto="hideModal",
-                    :uploadImageState='uploadImage',
-                    @showUpload='showImageModal',
-                    :photo='photo',
-                    :uploadedPhoto='uploadedPhoto')
+    include chat_box.pug
 
 </template>
 
